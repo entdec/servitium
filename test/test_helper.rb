eed61f7a14d0b0c37d7108082c7fadb62200ca4d
Mark Cornelissen <mark@boxture.com>
2021-06-02 12:11:16 +0200

    Do not log ActiveJob to STDOUT during tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,3 +7,6 @@ require 'minitest/autorun'
 require 'pry'
 
 require 'test_helpers/servitium_helpers'
+
+# We don't want logs to be spewed out to STDOUT during tests
+ActiveJob::Base.logger = Logger.new(nil)
