d7ee80c4c1ae95141314d845b7c9cdafde3796ff
Mark Cornelissen <mark@boxture.com>
2021-10-25 19:20:23 +0200

    Do not assign variable that is not used

--- test/servitium_test.rb
+++ test/servitium_test.rb
@@ -62,7 +62,7 @@ class ServitiumTest < ActiveSupport::TestCase
 
   def test_a_context_failure_error_from_an_inside_service_ensures_a_rollback
     assert_raises(Servitium::ContextFailure) do
-      context = TestTransactionalService.perform(servitium: 'pizza')
+      TestTransactionalService.perform(servitium: 'pizza')
     end
 
     assert_equal 0, Message.count
